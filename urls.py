1eb93705c3b153cb949b2c583faf73e4b1595301
rui xia <ruixia21@gmail.com>
2011-11-20 22:54:43 -0800

    working on login and registration

---
+++ urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib.auth.views import login, logout
+from wordout.views import *
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^$', main_page),
+    (r'^login/$', login),
+    (r'^logout/$', logout_page),
+    (r'^register/$', register_page),
+    # Examples:
+    # url(r'^$', 'django_wordout.views.home', name='home'),
+    # url(r'^django_wordout/', include('django_wordout.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
---
+++ wordout/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render_to_response
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from wordout.forms import *
+from django.template import RequestContext
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth import authenticate, login as auth_login
+
+def main_page(request):
+    return render_to_response(
+            'main_page.html',
+            dict(user=request.user)
+        )
+
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+def register_page(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                    username=form.cleaned_data['username'],
+                    password=form.cleaned_data['password1'],
+                    email=form.cleaned_data['email']
+                    )
+            new_user = authenticate(username=request.POST['username'], password=request.POST['password1'])
+            auth_login(request, new_user)
+            return HttpResponseRedirect('/')
+    else:
+        form = RegistrationForm()
+    return render_to_response('registration/register.html', dict(form = form), context_instance=RequestContext(request))
