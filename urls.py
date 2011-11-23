6d5ff5ace97852530394c8888362c71c105bf5d2
rui xia <ruixia21@gmail.com>
2011-11-22 16:19:04 -0800

    add change password feature

--- urls.py
+++ urls.py
@@ -1,14 +1,18 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.contrib.auth.views import login, logout, password_reset, password_reset_done, password_reset_confirm, password_reset_complete
+from django.contrib.auth.views import login, logout, password_reset, password_reset_done, password_reset_confirm, password_reset_complete, password_change, password_change_done
 
 from wordout.views import *
 from django.contrib import admin
 admin.autodiscover()
 from wordout.custom_decorator import anonymous_required
+from django.contrib.auth.decorators import login_required
+
+
+
 
 urlpatterns = patterns('',
     (r'^$', main_page),
-    (r'^login/$', anonymous_required(login)),
+    (r'^accounts/login/$', anonymous_required(login)),
     (r'^logout/$', logout_page),
     (r'^register/$', anonymous_required(register_page)),
     # Examples:
@@ -17,10 +21,18 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    
 
+    #django plugin for change password and reset password
+    (r'^password/change/$', password_change),
+    (r'^password/change/done$', password_change_done),
     (r'^password_reset/$', password_reset),
     (r'^password_reset/done/$', password_reset_done),
     (r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm),
     (r'^reset/done/$', password_reset_complete),
+
+
+
     url(r'^admin/', include(admin.site.urls)),
+
 )
