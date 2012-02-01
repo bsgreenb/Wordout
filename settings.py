95062ee34a53d6ebbfff7cc14d45b6cec43ea3e4
Ben Greenberg <bsgreenb@gmail.com>
2012-02-01 14:05:52 -0800

    Some english changes, and also abstracted the hostname

--- settings.py
+++ settings.py
@@ -9,6 +9,16 @@ ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
+#Defaults + request processior
+TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
+ "django.core.context_processors.debug",
+ "django.core.context_processors.i18n",
+ "django.core.context_processors.media",
+ "django.core.context_processors.static",
+ "django.contrib.messages.context_processors.messages",
+ "django.core.context_processors.request",)
+
+
 MANAGERS = ADMINS
 
 DATABASES = {
